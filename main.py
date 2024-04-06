9d59edd20bf2dea6d8b60e167dc926f7f8a8d42b
Alina <alina17ru@icloud.com>
2024-04-06 23:04:20 +0300

    deleting overdue events

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import tg_parser
 import sirius_parser_ft
 import park_sirius_parser
 import sqlite3
+from datetime import datetime
+
 
 words_in_events = ['фестиваль', 'состоится', 'мероприятие', 'концерт', 'билеты', 'выходные', 'соревнование']
 
@@ -35,8 +37,6 @@ for ev in data:
     ev_information = (ev[0], ev[2], ev[1], ev[3])
     data_to_insert.append(tuple(ev_information))
 
-with open('text', 'w') as file:
-    file.write('an')
 
 for elem in data_to_insert:
     q = f"SELECT id FROM events WHERE event = '{elem[0]}' AND date = '{elem[1]}'"
@@ -44,6 +44,13 @@ for elem in data_to_insert:
     existing_ev= cursor.fetchone()
     if not existing_ev:
         cursor.execute(f"INSERT INTO events (event, date, location, price) VALUES (?, ?, ?, ?)", elem)
+
+db.commit()
+
+# удаление просроченных событий
+current_date = datetime.now().date()
+sql = f"DELETE FROM events WHERE date < '{current_date}'"
+cursor.execute(sql)
 
 db.commit()
 db.close()
