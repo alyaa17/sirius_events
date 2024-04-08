b1b5c2aedc619745b27753ec9d1f5f10615a44e7
Alina <alina17ru@icloud.com>
2024-04-08 10:57:50 +0300

    add timezone

--- run.py
+++ run.py
@@ -1,13 +1,14 @@
 import schedule
 import time
 import subprocess
+import pytz
 
 
 def run_script():
     subprocess.run(["python3", "main.py"])
 
 
-schedule.every().day.at("00:00").do(run_script)
+schedule.every().day.at("10:44", "Europe/Moscow").do(run_script)
 
 while True:
     schedule.run_pending()
