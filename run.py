ff6ed100ddd51848f64c0450de038992320f1b77
Alina <alina17ru@icloud.com>
2024-04-08 13:15:54 +0300

    fix run

--- run.py
+++ run.py
@@ -3,12 +3,14 @@ import time
 import subprocess
 import pytz
 
+subprocess.run(["python3", "main.py"])
+
 
 def run_script():
     subprocess.run(["python3", "main.py"])
 
 
-schedule.every().day.at("10:44", "Europe/Moscow").do(run_script)
+schedule.every().day.at("00:00", "Europe/Moscow").do(run_script)
 
 while True:
     schedule.run_pending()
